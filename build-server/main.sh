870e1f95e0f7d1f53df52e60057c83c883d1eedb
anuragK-24 <24sept.anurag@gmail.com>
2024-03-27 23:47:42 +0530

    Remove space error from shell file

--- build-server/main.sh
+++ build-server/main.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-export GIT_REPOSITORY_URL = "$GIT_REPOSITORY_URL"
-=======
 export GIT_REPOSITORY_URL="$GIT_REPOSITORY_URL"
->>>>>>> df4ba13 (Added the Reverse proxy and api-server)
 # It'll export the git repository url to the environment variable
 
 git clone $GIT_REPOSITORY_URL /home/app/output
 # Above code will clone the repository to the output folder                                 
-<<<<<<< HEAD
 
 exec node script.js 
 # It'll execute the script.js file
 
-=======
-
-exec node script.js 
-# It'll execute the script.js file
->>>>>>> df4ba13 (Added the Reverse proxy and api-server)
